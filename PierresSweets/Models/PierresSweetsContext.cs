a5b23acdeaf9a3dedf7d8580659610eaaf01ef01
Lucas Mollerstuen <115112679+oz-mollerstuen@users.noreply.github.com>
2023-01-13 10:28:07 -0800

    Add account controller

--- PierresSweets/Models/PierresSweetsContext.cs
+++ PierresSweets/Models/PierresSweetsContext.cs
@@ -3,8 +3,17 @@ using Microsoft.EntityFrameworkCore;
 
 namespace PierresSweets.Models
 {
-    public class PierresSweetsContext : IdentityDbContext<ApplicationUser>
+  public class PierresSweetsContext : IdentityDbContext<ApplicationUser>
+  {
+    public DbSet<Flavor> Flavors { get; set; }
+    public DbSet<Treat> Treats { get; set; }
+    public DbSet<FlavorTreat> FlavorTreat { get; set; }
+
+    public PierresSweetsContext(DbContextOptions options) : base(options) { }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-   
+      optionsBuilder.UseLazyLoadingProxies();
     }
+  }
 }
